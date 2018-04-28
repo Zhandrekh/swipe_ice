a80feac19cab13a2b42310a835a92169ecf6afc6
Naomi <naomi.gaye@orange.fr>
2018-04-28 02:12:59 +0200

    music

---
+++ Assets/DontDestroy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake() {
+		/*GameObject[] obs = GameObject.FindGameObjectsWithTag ("music");
+		if (obs.Length > 1)
+			Destroy (this.gameObject);*/
+
+		DontDestroyOnLoad (this.gameObject);
+	}
+
+}
